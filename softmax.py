027397c5ea69601c54a7c91b0ce759bd44c4ee82
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-12-09 22:18:35 +0530

    Softmax

---
+++ softmax.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import cmath
+def integration(x):
+    a=np.exp(x)
+    return sum(a)
+    
+def softmax(x):
+    return np.exp(x)/integration(x)
+    
+x = np.linspace(-10, 10)
+plt.plot(x, softmax(x))
+plt.axis('tight')
+plt.title('Activation Function :softmax')
+plt.show()
